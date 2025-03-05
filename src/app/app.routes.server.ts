5313864ec17398eccb13da13fad311505032bc50
ayaamar2001 <129336185+ayaamar200@users.noreply.github.com>
2025-03-05 12:22:26 +0200

    .

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:productId',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
